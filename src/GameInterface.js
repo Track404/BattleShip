482cabde2fddb19e3eb6156e84f6e907b69fc8bd
Track404 <111713000+Track404@users.noreply.github.com>
2024-07-16 18:22:12 +0200

    record shot on gameboard UI and tell if game done

--- src/GameInterface.js
+++ src/GameInterface.js
@@ -1,3 +1,5 @@
+/* eslint-disable operator-linebreak */
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-plusplus */
 /* eslint-disable no-unused-vars */
 import Player from './player';
@@ -9,7 +11,7 @@ import Icon from './images/battleshipLogo.jpg';
 const game = new Player();
 const destroyer = new Ship(3);
 const carrier = new Ship(5);
-game.player.placeShip(destroyer, 0, 0, 'vertical');
+game.player.placeShip(destroyer, 6, 4, 'horizontal');
 game.player.placeShip(carrier, 0, 2, 'vertical');
 game.computer.placeShip(destroyer, 0, 0, 'vertical');
 game.computer.placeShip(carrier, 0, 2, 'vertical');
@@ -31,6 +33,12 @@ for (let i = 0; i < game.player.grid.length; i++) {
     pixel.setAttribute('row', i);
     pixel.setAttribute('column', j);
     div.appendChild(pixel);
+    if (game.player.grid[i][j] instanceof Ship) {
+      pixel.style.backgroundColor = 'blue';
+      if (game.player.grid[i][j].sunkShip) {
+        pixel.style.backgroundColor = 'yellow';
+      }
+    }
   }
   playerGrid.appendChild(div);
 }
@@ -45,6 +53,40 @@ for (let i = 0; i < game.computer.grid.length; i++) {
     pixel.classList.add('pixel');
 
     div.appendChild(pixel);
+    if (game.computer.grid[i][j] instanceof Ship) {
+      pixel.style.backgroundColor = 'blue';
+    }
   }
   computerGrid.appendChild(div);
 }
+const gridSquare = document.querySelectorAll('.pixel');
+
+gridSquare.forEach((pixel) => {
+  pixel.addEventListener('click', () => {
+    const row = pixel.getAttribute('row');
+    const column = pixel.getAttribute('column');
+    game.player.receiveAttack(row, column);
+    if (
+      pixel.style.backgroundColor === 'red' ||
+      pixel.style.backgroundColor === 'grey'
+    ) {
+      return;
+    }
+    if (game.player.grid[row][column] instanceof Ship) {
+      pixel.style.backgroundColor = 'red';
+      pixel.classList.add('hit');
+      if (game.player.grid[row][column].sunkShip === true) {
+        const sunk = document.querySelectorAll('.hit');
+        sunk.forEach((hitPixel) => {
+          hitPixel.style.backgroundColor = 'yellow';
+        });
+      }
+    } else {
+      pixel.style.backgroundColor = 'grey';
+      pixel.classList.add('miss');
+    }
+    if (game.player.allHit === 0) {
+      alert('player win game');
+    }
+  });
+});
